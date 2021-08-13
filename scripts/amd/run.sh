f184b432669622bd21ec483adba8a365b79492b5
Michael Melesse <micmelesse@gmail.com>
2021-08-12 19:12:56 -0500

    add scripts from enable_gbn

---
+++ scripts/amd/run.sh
@@ -0,0 +1,5 @@
+clear
+sh scripts/amd/clean.sh
+sh scripts/amd/deps.sh
+sh scripts/amd/build.sh
+sh scripts/amd/test.sh
---
+++ scripts/amd/test.sh
@@ -0,0 +1,13 @@
+python scripts/amd/apex_gbn_add_relu_test.py
+
+# gdb -ex "set breakpoint pending on" \
+#     -ex 'break _fwdKernelLauncher' \
+#     -ex 'run' \
+#     --args python scripts/amd/apex_gbn_add_relu_test.py
+
+# python -m pdb \
+#     -c "b BatchNorm2d_NHWC.forward" \
+#     -c c scripts/amd/apex_gbn_add_relu_test.py
+
+# python -m pdb \
+#     -c c scripts/amd/apex_gbn_add_relu_test.py
