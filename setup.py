df833cb226e98f5e400397d7add233897c67bd19
yaoyu-33 <54727607+yaoyu-33@users.noreply.github.com>
2022-11-22 11:32:48 -0800

    Update megatron fused softmax follow megatron-lm (#1539)
    * Update megatron fused softmax follow megatron-lm
    
    Signed-off-by: Yu Yao <yuya@nvidia.com>
    
    * Add mask=None support in scaled_masked_softmax
    
    Signed-off-by: Yu Yao <yuya@nvidia.com>
    
    * Update setup.py for scaled_softmax_cuda
    
    Signed-off-by: Yu Yao <yuya@nvidia.com>
    
    * Add tests for fused_scale_softmax (mask=None)
    
    Signed-off-by: Yu Yao <yuya@nvidia.com>
    
    * Assert grad equal in fused softmax test
    
    Signed-off-by: Yu Yao <yuya@nvidia.com>
    
    * Revert "Assert grad equal in fused softmax test"
    
    Signed-off-by: Yu Yao <yuya@nvidia.com>
    
    Signed-off-by: Yu Yao <yuya@nvidia.com>
    Co-authored-by: Yu Yao <yuya@nvidia.com>

--- setup.py
+++ setup.py
@@ -307,6 +307,39 @@ if "--cuda_ext" in sys.argv:
                                         os.path.join(this_dir, 'csrc/megatron')],
                           extra_compile_args={'cxx': ['-O3'] + version_dependent_macros,
                                               'nvcc':nvcc_args_transformer if not IS_ROCM_PYTORCH else hipcc_args_transformer}))
+        ext_modules.append(
+            CUDAExtension(name='scaled_softmax_cuda',
+                          sources=['csrc/megatron/scaled_softmax.cpp',
+                                   'csrc/megatron/scaled_softmax_cuda.cu'],
+                          include_dirs=[os.path.join(this_dir, 'csrc'),
+                                        os.path.join(this_dir, 'csrc/megatron')],
+                          extra_compile_args={'cxx': ['-O3'] + version_dependent_macros,
+                                              'nvcc':nvcc_args_transformer if not IS_ROCM_PYTORCH else hipcc_args_transformer}))
+        # TODO (hubertlu): support fused_weight_gradient_mlp_cuda for ROCm
+        if not IS_ROCM_PYTORCH:
+            if bare_metal_version >= Version("11.0"):
+                cc_flag = []
+                cc_flag.append("-gencode")
+                cc_flag.append("arch=compute_70,code=sm_70")
+                cc_flag.append("-gencode")
+                cc_flag.append("arch=compute_80,code=sm_80")
+                if bare_metal_version >= Version("11.1"):
+                    cc_flag.append("-gencode")
+                    cc_flag.append("arch=compute_86,code=sm_86")
+                if bare_metal_version >= Version("11.8"):
+                    cc_flag.append("-gencode")
+                    cc_flag.append("arch=compute_90,code=sm_90")
+
+            ext_modules.append(
+            CUDAExtension(name='fused_weight_gradient_mlp_cuda',
+                          sources=['csrc/megatron/fused_weight_gradient_dense.cpp',
+                                   'csrc/megatron/fused_weight_gradient_dense_cuda.cu',
+                                   'csrc/megatron/fused_weight_gradient_dense_16bit_prec_cuda.cu'],
+                          include_dirs=[os.path.join(this_dir, 'csrc'),
+                                        os.path.join(this_dir, 'csrc/megatron')],
+                          extra_compile_args={'cxx': ['-O3'] + version_dependent_macros,
+                                              'nvcc':append_nvcc_threads(nvcc_args_transformer + ['--use_fast_math'] + cc_flag) 
+                                              if not IS_ROCM_PYTORCH else hipcc_args_transformer}))
 
 
 if "--bnp" in sys.argv or "--cuda_ext" in sys.argv:
